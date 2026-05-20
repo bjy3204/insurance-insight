54060045fa5e3ec2a318ad33af6139db6d65c798
배지연 <bjy3204@gmail.com>
2026-05-20 11:51:13 +0900

    update

--- app/saving-calculator/page.tsx
+++ app/saving-calculator/page.tsx
@@ -479,9 +479,18 @@ const principal = monthly * months;
             <Link
               href="/"
               className="
-                w-11 h-11 rounded-2xl border border-gray-300 bg-white
-                flex items-center justify-center
-              "
+  absolute
+  left-0
+  w-11
+  h-11
+  rounded-2xl
+  border
+  border-gray-300
+  bg-white
+  flex
+  items-center
+  justify-center
+"
             >
               <ArrowLeft className="w-5 h-5 text-black" />
             </Link>
