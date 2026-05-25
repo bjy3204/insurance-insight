f4f28c0cf0d0ec4c58c6f6c0fb792ff20cdc35fc
배지연 <bjy3204@gmail.com>
2026-05-25 13:07:39 +0900

    add robots

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -7,13 +7,45 @@ export default function sitemap(): MetadataRoute.Sitemap {
       lastModified: new Date(),
       priority: 1,
     },
+
+    {
+      url: "https://insurance-insight.vercel.app/insurance-system",
+      lastModified: new Date(),
+      priority: 0.9,
+    },
+
+    {
+      url: "https://insurance-insight.vercel.app/customer-center",
+      lastModified: new Date(),
+      priority: 0.9,
+    },
+
+    {
+      url: "https://insurance-insight.vercel.app/product-public",
+      lastModified: new Date(),
+      priority: 0.9,
+    },
+
     {
       url: "https://insurance-insight.vercel.app/calculator",
       lastModified: new Date(),
+      priority: 0.9,
+    },
+
+    {
+      url: "https://insurance-insight.vercel.app/money-value",
+      lastModified: new Date(),
       priority: 0.8,
     },
+
     {
-      url: "https://insurance-insight.vercel.app/customer",
+      url: "https://insurance-insight.vercel.app/saving-calculator",
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+
+    {
+      url: "https://insurance-insight.vercel.app/pension-calculator",
       lastModified: new Date(),
       priority: 0.8,
     },
